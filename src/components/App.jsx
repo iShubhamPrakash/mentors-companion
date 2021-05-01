f3a16a5391648a44a3d5f1298404e918f4b45cfc
Shubham Prakash <shubham.prakash2308@gmail.com>
2021-05-02 04:11:34 +0530

    Create a file to store default feedback data for dev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import Home from "../pages/Home";
-import NOT_FOUND from "../pages/NOT_FOUND";
 import { Feedback } from "./Feedback";
 import { InputBox } from "./InputBox";
-import { Nav } from "./Nav";
+
+import { Grid, GridItem, Heading } from "@chakra-ui/react";
+import DataPieChart from "./DataPieChart";
+import { defaultData } from "../default_data.js";
 
 function App() {
-	const [feedbackdata, setFeedbackdata] = useState([]);
+	const [feedbackdata, setFeedbackdata] = useState(defaultData);
 	return (
 		<div className="app">
-			<Nav />
-			<InputBox setFeedbackdata={setFeedbackdata} />
-
-			<div className="feedback_container">
-				{feedbackdata.map((data) => (
-					<Feedback data={data} />
-				))}
-			</div>
+			<Grid
+				templateRows="repeat(1, 1fr)"
+				templateColumns="repeat(7, 1fr)"
+				gap={4}
+			>
+				<GridItem colSpan={5}>
+					<Heading pl={4} as="h3" size="md">
+						Reviews History
+					</Heading>
+					<div className="feedback_container">
+						{feedbackdata.map((data) => (
+							<Feedback data={data} />
+						))}
+					</div>
+				</GridItem>
+				<GridItem colSpan={2} p={1} pt={10}>
+					{" "}
+					<InputBox setFeedbackdata={setFeedbackdata} />
+					<DataPieChart feedbackdata={feedbackdata} />
+				</GridItem>
+			</Grid>
 		</div>
 	);
 }
---
+++ src/default_data.js
@@ -0,0 +1,2 @@
+// For development, put the JSOn data obtained from https://review-api.udacity.com/api/v1/me/student_feedbacks
+export const defaultData = [];
